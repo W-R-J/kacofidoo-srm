d2352b0c797bce9f70dced8b6723626d71520a62
jjeffcaii@outlook.com <jjeffcaii@outlook.com>
2013-09-21 12:55:22 +0800

    update: mysql schema ddl

--- srm-orm/src/main/resources/ddl/mysql-schema.sql
+++ srm-orm/src/main/resources/ddl/mysql-schema.sql
@@ -1,35 +1,39 @@
-drop table if exists srm_user;
-create table srm_user(
-id int(19) auto_increment primary key,
-name varchar(255) not null unique,
-password varchar(60),
-nickname varchar(255),
-birthday TIMESTAMP,
-gender int(1),
-company_id int(19),db
-company_admin_flag int(1),
-system_admin_flag int(1),
-
-status int(1) null default 0,
-create_time timestamp,
-modify_time timestamp,
-creator int(19),
-modifier int(19)
-) engine=innodb;
-
-drop table if exists srm_company;
-create table srm_company()
-id int(19) auto_increment primary key,
-name varchar(255) not null unique,
-description varchar(2000),
-website varchar(255),
-address varchar(255),
-zipcode varchar(6),
-
-status int(1) null default 0,
-create_time timestamp,
-modify_time timestamp,
-creator int(19),
-modifier int(19)
-)engine=innodb;
-
+DROP TABLE
+    IF EXISTS srm_user;
+CREATE TABLE
+    srm_user
+    (
+        id INT(19) auto_increment PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(60),
+        nickname VARCHAR(255),
+        birthday TIMESTAMP,
+        gender INT(1),
+        company_id INT(19),
+        company_admin_flag INT(1),
+        system_admin_flag INT(1),
+        status INT(1) NULL DEFAULT 0,
+        create_time TIMESTAMP,
+        modify_time TIMESTAMP,
+        creator INT(19),
+        modifier INT(19)
+    )
+    engine=innodb;
+DROP TABLE
+    IF EXISTS srm_company;
+CREATE TABLE
+    srm_company
+    (
+        id INT(19) auto_increment PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        description VARCHAR(2000),
+        website VARCHAR(255),
+        address VARCHAR(255),
+        zipcode VARCHAR(6),
+        status INT(1) NULL DEFAULT 0,
+        create_time TIMESTAMP,
+        modify_time TIMESTAMP,
+        creator INT(19),
+        modifier INT(19)
+    )
+    engine=innodb;
